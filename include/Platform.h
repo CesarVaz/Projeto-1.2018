398703208e4a02297751185d5a24766d4ccbfb87
CesarVaz <39285151+CesarVaz@users.noreply.github.com>
2018-05-15 19:03:56 -0300

    Add files via upload

---
+++ include/Platform.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Platform
+{
+    public:
+        Platform(sf::Texture* texture, sf::Vector2f size, sf::Vector2f position);
+        virtual ~Platform();
+
+        void Draw(sf::RenderWindow& window);
+
+        sf::RectangleShape getBody()
+        {
+            return body;
+        }
+
+        bool get_isActivePlatform();
+        void set_isActivePlatform(bool activePlatform);
+        void bodyMove(float moveX, float moveY);
+
+    private:
+        sf::RectangleShape body;
+        bool isActivePlatform;
+};
